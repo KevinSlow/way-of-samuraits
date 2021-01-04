e7ae1792ec96236c2fa6204d85403c7c6ea118d5
Sergey <kurilovsergey15@gmail.com>
2021-01-04 02:55:39 +0200

    99

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,36 +1,30 @@
 import React from "react";
-import {
-    addDialogActionCreator,
-} from "../../redux/dialogsReducer";
+import { addDialogActionCreator } from "../../redux/dialogsReducer";
 import Dialogs from "./Dialogs";
-import {RootStoreType} from "../../redux/store";
-import {connect} from "react-redux";
-import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import { RootStoreType } from "../../redux/store";
+import { connect } from "react-redux";
+import { withAuthRedirect } from "../../hoc/withAuthRedirect";
 import { compose } from "redux";
 
 type dialogsContainerPropsType = {
-    store: RootStoreType
+  store: RootStoreType;
 };
 
 const mapStateToProps = (state: any) => {
-    return{
-        dialogsPage: state.dialogsPage,
-    }
-}
+  return {
+    dialogsPage: state.dialogsPage,
+  };
+};
 
 const mapDispatchToProps = (dispatch: any) => {
-    return{
-        sendMessage: (newMessageBody:string) => {
-            dispatch(addDialogActionCreator(newMessageBody));
-        },
-    }
-}
-
-
-
-
+  return {
+    sendMessage: (newMessageBody: string) => {
+      dispatch(addDialogActionCreator(newMessageBody));
+    },
+  };
+};
 
 export default compose<any>(
-    connect(mapStateToProps, mapDispatchToProps),
-    withAuthRedirect
+  connect(mapStateToProps, mapDispatchToProps),
+  withAuthRedirect
 )(Dialogs);
